9ba4ac93785b8ac528b03baf63d2c253a1c0c1d8
Stelios Taliadwros <steliosevoix@gmail.com>
2024-05-18 15:34:22 +0300

    init: Initial project setup

--- jobs-api/server.js
+++ jobs-api/server.js
@@ -3,6 +3,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connect configuration  
+const connectToDB = require('./db/connect');
+const uri = process.env.URI
+const db_name = process.env.DB_NAME
+const url = `${uri}/${db_name}`
+
+// routes imports 
+const authRoute = require("./routes/auth")
+const jobsRoute = require('./routes/jobs')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +21,9 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/jobs',jobsRoute)
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +32,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectToDB(url)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
